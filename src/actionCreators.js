b967915c9b1db4f4f96166ea8759cc28b7f3f26c
Nestor Felipe <necrifede@gmail.com>
2019-04-23 13:54:17 +0200

    fix #6 remove console.logs

--- src/actionCreators.js
+++ src/actionCreators.js
@@ -12,9 +12,7 @@ export function updateNumericValue(value) {
 
 export function updateResult(value) {
   return async dispatch => {
-    console.log('value: ', value)
     const response = await axios.get(`${host}/phonenumber?value=${value}`)
-    console.log('response.data.result: ', response.data.result)
     dispatch({
       type: 'UPDATE_RESULTS',
       payload: response.data.result || []
